883b9b85bcef028725159de9d244a6308d64e738
남경화 <khnam5@naver.com>
2023-01-03 16:12:51 +0900

    time

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import ExpenseAmount from './Expenses';
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+ 
+
+    return (
+        <div className='expense-item'>
+        <ExpenseDate date={props.date}/>
+          
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                {/* <ExpenseAmount amount={props.amount}/> */}
+                <div className='expense-item__price'>₩{props.amount}</div>
+            </div>
+        </div>)
+}
+
+export default ExpenseItem;
