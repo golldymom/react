7556988b93562850b4eab7cfda64241314962fd9
남경화 <khnam5@naver.com>
2023-01-06 19:04:57 +0900

    redux

--- summary/code/19-finished/src/index.js
+++ summary/code/19-finished/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { FavoritesContextProvider } from './store/favorites-context';
-
+// 컴포넌트 함수를 가져와야 해서 중괄호 사용
 ReactDOM.render(
   <FavoritesContextProvider>
     <BrowserRouter>
